cd3e67f058d1078285499a573585f05c06ff176d
Austin Reilly <funcychaos@gmail.com>
2025-04-09 23:19:03 -0700

    feat(layout): add some main layout styles and files

---
+++ javascript/size.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', setVhUnit)
+
+function setVhUnit(){
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+setVhUnit()
